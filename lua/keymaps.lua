9aab2e710de1966ae5e7c8145e1ed7c0485e7b49
slownin <kiz403@gmail.com>
2026-05-23 23:44:57 +0900

    Telescope関連のキーマップ追加

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,3 +19,8 @@ map('n', '<Leader>mp', '<Cmd>MarkdownPreviewToggle<CR>', { noremap = true, silen
 map('n', '<S-h>', '<Cmd>bprevious<CR>', { noremap = true })
 map('n', '<S-l>', '<Cmd>bnext<CR>', { noremap = true })
 map('n', '<Leader>bd', '<Cmd>bdelete<CR>', { noremap = true })
+
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = 'Telescope help tags' })
