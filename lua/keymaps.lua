6e44db94112c11b1dc29966613ae78f295d13998
slownin <kiz403@gmail.com>
2026-05-23 23:52:30 +0900

    整理

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,16 +10,21 @@ map('n', '<Leader>l', '<C-w>l', { noremap = true, silent = true })
 map('n', '<Leader>s', '<Cmd>sp<CR>', { noremap = true, silent = true })
 map('n', '<Leader>v', '<Cmd>vs<CR>', { noremap = true, silent = true })
 
+-- 雑多
 map('i', 'jj', '<ESC>', { noremap = true })
 
-map('n', '<Leader>b', '<Cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
-
-map('n', '<Leader>mp', '<Cmd>MarkdownPreviewToggle<CR>', { noremap = true, silent = true })
-
+-- バッファ
 map('n', '<S-h>', '<Cmd>bprevious<CR>', { noremap = true })
 map('n', '<S-l>', '<Cmd>bnext<CR>', { noremap = true })
 map('n', '<Leader>bd', '<Cmd>bdelete<CR>', { noremap = true })
 
+-- NvimTree
+map('n', '<Leader>b', '<Cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- MarkdownPreview
+map('n', '<Leader>mp', '<Cmd>MarkdownPreviewToggle<CR>', { noremap = true, silent = true })
+
+-- Telescope
 vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' })
