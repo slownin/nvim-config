9dedef82b1064b8c36b962e921e4d6cd5e02558f
slownin <kiz403@gmail.com>
2026-07-01 09:40:48 +0900

    nvim-treeの設定変更

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-tree/nvim-tree.lua",
   opts = {
+    sync_root_with_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+    },
     filesystem_watchers = {
       enable = false,
     },
